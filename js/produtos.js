de320d71f6a37687a93c9092ad58ea318fc67240
Caius Lopes Reyes Hanusz <caius58382316@edu.df.senac.br>
2026-03-27 15:53:38 -0300

    adição da pasta js e data

---
+++ js/produtos.js
@@ -0,0 +1,88 @@
+/*
+=======================================
+1 - PEGAR O ELEMENTO(TAG) DO HTML
+=======================================
+*/
+
+// Cria uma variável constante com a referência da tag HTML
+const buscaProdutos = document.querySelector("#buscarProdutos");
+const listaProdutos = document.querySelector("#listaProdutos");
+
+/*
+=======================================
+2 - CRIAR UMA LISTA VAZIA PARA GUARDAR OS PRODUTOS
+=======================================
+*/
+let produtos = []; // aqui vai ficar a lista carregada do JSON
+
+/*
+=======================================
+3 - FUNÇÃO PARA CARREGAR O JSON COM OS PRODUTOS
+=======================================
+*/
+async function carregarProdutos(){
+    // buscar o arquivo produtos.json (como se fosse uma mini API)
+    const resposta = await fetch("../data/produtos.json");
+
+    // Tranformar o JSON em dados que JS entende
+    Produtos = await resposta.json();
+
+    // depois de carregar, já renderiza na tela
+    renderizarProdutos(produtos);
+
+}
+
+
+/*
+=======================================
+4 - FUNÇÃO PARA CRIAR OS CARDS DOS PRODUTOS NA TELA (DOM)
+=======================================
+*/
+function renderizarProdutos(lista){
+    // limpa o conteúdo na tela antes de desenhar de novo
+    listaProdutos.innerHTML="";
+
+    // para cada Produtos da lista, cria um card
+    lista.forEach((produto) => {
+        // cria uma element/tag DIV
+        const card = document.createElement("div");
+
+        // coloca uma classe (para o CSS estilizar)
+        card.classList.add("card-produto");
+
+        // coloca o conteúdo dentro do CARD
+        card.innerHTML = `
+            <h3> ${produto.título} </h3>
+            <img src=${produto.img} width="55" heigth="55">
+            <p> ${produto.desc} </p>
+            <p><strong>CH: <strong> ${produto.ch}  </p>
+            <a href=${produto.url}>
+            <button class="btn-detalhes">Ver Detalhes</button>
+            </a>
+        `;
+        // coloca o card dentro da lista
+        listaProdutos.appendChild(card);
+
+    });
+
+}
+
+
+/*
+=======================================
+5 - BUSCA : FILTRAR PRODUTOS NA QUANDO DIGITAR
+=======================================
+*/
+buscaProdutos.addEventListener("input",function(){
+    const texto = buscaProdutos.value.toLowerCase();
+
+    const filtrados = Produtos.filter((produto) => 
+        produto.título.toLowerCase().includes(texto)
+    );
+
+    renderizarProdutos(filtrados);
+});
+
+
+
+carregarProdutos();
