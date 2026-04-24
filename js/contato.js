31e01dc79a54cdaa4dbcb2130dd2ca82ad5ebe52
Caius Lopes Reyes Hanusz <caius58382316@edu.df.senac.br>
2026-04-24 17:40:30 -0300

    atualiza

--- js/contato.js
+++ js/contato.js
@@ -2,6 +2,10 @@
 // CONTATO.JS — Envio do formulário para o servidor
 // ================================================
 
+//API_URL = "http://localhost:3000/"
+
+API_URL = "https://projeto-cafezes.onrender.com"
+
 // Pega o formulário pelo ID definido no contato.html
 const form = document.getElementById("formContato");
 
@@ -22,7 +26,7 @@ form.addEventListener("submit", async function (event) {
   try {
     // Envia os dados via POST para o servidor Node (server.js)
     // Se mudar a porta do servidor, atualize o número abaixo (3000)
-    await fetch("http://localhost:3000/contatos", {
+    await fetch(`http://localhost:3000/${API_URL}`, {
       method:  "POST",
       headers: { "Content-Type": "application/json" },
       body:    JSON.stringify(dados)
